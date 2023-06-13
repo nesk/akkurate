2425869a5c50b9f7e095a88710ba300a87330a23
Johann Pardanaud <johann@pardanaud.com>
2023-06-13 13:36:06 +0200

    add tests for conversions from KSP models to the KotlinPoet ones

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -13,8 +13,14 @@ dependencies {
     implementation("com.squareup:kotlinpoet:1.14.2")
     implementation("com.google.devtools.ksp:symbol-processing-api:1.8.21-1.0.11")
     implementation(project(":library"))
+
+    testImplementation(kotlin("test"))
 }
 
 kotlin {
     jvmToolchain(11)
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
