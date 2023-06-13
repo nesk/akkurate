153f0bcbf7614410046be65689badf82cc86f306
Johann Pardanaud <johann@pardanaud.com>
2023-06-13 13:34:52 +0200

    support nested classes

--- plugin/src/main/kotlin/KspToPoetConverters.kt
+++ plugin/src/main/kotlin/KspToPoetConverters.kt
@@ -4,18 +4,28 @@ import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.TypeName
 
-internal fun KSDeclaration.toClassName(): ClassName = ClassName(packageName.asString(), simpleName.asString())
+internal fun KSDeclaration.toClassName(): ClassName {
+    val simpleNames = mutableListOf(simpleName)
+
+    var parent = parentDeclaration
+    while (parent != null) {
+        simpleNames += parent.simpleName
+        parent = parent.parentDeclaration
+    }
+
+    return ClassName(packageName.asString(), *simpleNames.reversed().map(KSName::asString).toTypedArray())
+}
 
 internal fun KSDeclaration.toTypeName(): TypeName = toClassName()
 
-internal fun KSTypeReference.toTypeName(): TypeName = resolve().toTypeName()
-
 internal fun KSType.toTypeName(): TypeName {
-    val className = ClassName(declaration.packageName.asString(), declaration.simpleName.asString())
+    val className = declaration.toClassName()
     val typeName = if (arguments.isEmpty()) className else {
         className.parameterizedBy(arguments.mapNotNull { it.type }.map { it.toTypeName() })
     }
     return typeName.copy(nullable = nullability == Nullability.NULLABLE)
 }
+
+internal fun KSTypeReference.toTypeName(): TypeName = resolve().toTypeName()
 
 internal fun KSPropertyDeclaration.toMemberName() = MemberName(parentDeclaration!!.toClassName(), simpleName.asString())
