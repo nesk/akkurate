86bfeb92ba1e1f95b9ed7cefc491f0f6fc2d3c8f
Johann Pardanaud <johann@pardanaud.com>
2023-06-18 08:22:19 +0200

    split the `Validatable` and `CompoundValidatable` definitions in two files

--- library/src/jvmMain/kotlin/dev/nesk/akkurate/validatables/CompoundValidatable.kt
+++ library/src/jvmMain/kotlin/dev/nesk/akkurate/validatables/CompoundValidatable.kt
@@ -1,22 +1,5 @@
 package dev.nesk.akkurate.validatables
 
-import dev.nesk.akkurate.Validator
-import dev.nesk.akkurate.constraints.Constraint
-
-public interface Validatable<T> {
-    public fun path(): List<String>
-    public fun unwrap(): T
-
-    // TODO: Should this be part of the public API?
-    public fun <V> createValidatable(value: V): Validatable<V>
-}
-
-public inline operator fun <T> Validatable<T>.invoke(block: Validatable<T>.() -> Unit): Unit = this.block()
-public inline fun <T> Validatable<T>.constrain(block: (value: T) -> Boolean): Constraint = TODO()
-public inline fun <T> Validatable<out T?>.constrainIfNotNull(block: (value: T) -> Boolean): Constraint = TODO()
-public fun <T> Validatable<T>.validateWith(validator: Validator.Runner<T>): Unit = TODO()
-public suspend fun <T> Validatable<T>.validateWith(validator: Validator.SuspendableRunner<T>): Unit = TODO()
-
 /*
     This way of managing compound validation doesn't allow to write `foo and bar {}` without parentheses around the compound: `(foo and bar) {}`
     This is due to Kotlin's precedence, if you write without parentheses, its like writing `foo and (bar {})`. We could solve this by applying the
