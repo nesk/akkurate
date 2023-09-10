af1605697c2e2398bfe417be529c0fb50a149a11
Johann Pardanaud <johann@pardanaud.com>
2023-09-11 00:06:57 +0200

    add Dokka to generate an API reference

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 plugins {
     kotlin("multiplatform") version "1.9.10" apply false
     kotlin("jvm") version "1.9.10" apply false
+    id("org.jetbrains.dokka") version "1.9.0"
+}
+
+repositories {
+    mavenCentral()
 }
