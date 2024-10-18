a2a3f23b82c6a6ba88405413fda75427db12132c
Johann Pardanaud <johann@pardanaud.com>
2024-10-18 18:22:06 +0200

    wip: core: make constructors public for `ConstraintViolationSet` and `ConstraintViolation` classes

--- akkurate-core/src/commonMain/kotlin/dev/nesk/akkurate/constraints/ConstraintViolationSet.kt
+++ akkurate-core/src/commonMain/kotlin/dev/nesk/akkurate/constraints/ConstraintViolationSet.kt
@@ -19,7 +19,7 @@ package dev.nesk.akkurate.constraints
 
 import dev.nesk.akkurate.Path
 
-public class ConstraintViolationSet internal constructor(private val messages: Set<ConstraintViolation>) : Set<ConstraintViolation> by messages {
+public class ConstraintViolationSet(private val messages: Set<ConstraintViolation>) : Set<ConstraintViolation> by messages {
     public val byPath: Map<Path, Set<ConstraintViolation>> by lazy { messages.groupBy { it.path }.mapValues { it.value.toSet() } }
 
     override fun equals(other: Any?): Boolean = messages == other
