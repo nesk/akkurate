3f713adcf60449b3d4377e14a77c51adb7abcdb0
bejibx <s.maks04@gmail.com>
2024-12-05 14:02:42 +0300

    Revert changes validatableOf signature for real

--- library/src/commonTest/kotlin/dev/nesk/akkurate/validatables/ValidatableTest.kt
+++ library/src/commonTest/kotlin/dev/nesk/akkurate/validatables/ValidatableTest.kt
@@ -23,6 +23,7 @@ import dev.nesk.akkurate.constraints.Constraint
 import dev.nesk.akkurate.constraints.ConstraintRegistry
 import dev.nesk.akkurate.constraints.ConstraintViolation
 import dev.nesk.akkurate.test.Validatable
+import kotlin.reflect.KProperty1
 import kotlin.test.*
 
 class ValidatableTest {
@@ -102,7 +103,8 @@ class ValidatableTest {
         // Arrange
         val parent = Validatable("foo" as String?, "string")
         // Act
-        val child = parent.validatableOf(String::length)
+        // FIXME: The cast is a workaround for https://youtrack.jetbrains.com/issue/KT-59493, it can be removed with KT v1.9.20
+        val child = parent.validatableOf(String::length as KProperty1)
         // Assert
         assertEquals(3, child.unwrap(), "The child validatable wraps the value of the property")
         assertEquals(listOf("string", "length"), child.path(), "The child validatable extends the parent path with the property name")
@@ -113,7 +115,8 @@ class ValidatableTest {
         // Arrange
         val parent = Validatable(null as String?, "string")
         // Act
-        val child = parent.validatableOf(String::length)
+        // FIXME: The cast is a workaround for https://youtrack.jetbrains.com/issue/KT-59493, it can be removed with KT v1.9.20
+        val child = parent.validatableOf(String::length as KProperty1)
         // Assert
         assertNull(child.unwrap(), "The child validatable wraps the value of the property")
         assertEquals(listOf("string", "length"), child.path(), "The child validatable extends the parent path with the property name")
