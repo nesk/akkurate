e5a838e7862c6fa3619f94857e26c35a85a0c8a2
bejibx <s.maks04@gmail.com>
2025-01-25 01:54:11 +0300

    Added two comments about gradle wrapper version configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ repositories {
     mavenCentral()
 }
 
+/*
+ * Configure default gradle wrapper task to validate currently used gradle wrapper version against value set in versions
+ * catalog. When value differs task will automatically update wrapper configuration file with version from catalog.
+ * After this gradle wrapper binary will be updated before next task execution.
+*/
 tasks.named<Wrapper>("wrapper") {
     gradleVersion = libs.versions.gradle.wrapper.get()
 }
